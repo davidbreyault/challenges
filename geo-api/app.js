444d43a86b1b5a95c33cb8fe9119069ec08f6f99
David <72495222+davidbreyault@users.noreply.github.com>
2021-03-17 14:30:00 +0100

    Add files via upload

---
+++ geo-api/app.js
@@ -0,0 +1,18 @@
+document.querySelector("#cp").addEventListener("input", function() {
+    if (this.value.length == 5) {
+        let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}&fields=nom,codesPostaux,population&format=json&geometry=centre`;
+        fetch(url).then(function(promise) {
+            promise.json().then(function(data) {
+                console.log(data);
+                let list = "<ul>";
+                for(city of data) {
+                    list += `<li><strong>${city.nom}</strong> ${city.population} habitants</li>`;
+                }
+                list += "</ul>";
+                document.querySelector("#result").innerHTML = list;
+            })
+        }).catch(function(error) {
+            console.log("Erreur : " + error);
+        })
+    }
+})
